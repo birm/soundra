f86a6fdd992076d240bee99ca4340613d2bbd71b
Ryan Birmingham <birm@rbirm.us>
2019-01-02 21:14:16 -0500

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,20 @@
-
+class Storage{
+  constructor(){
+    let a
+  }
+  saveTrack(){
+    return 0
+  }
+  getTrack(uuid){
+    return 0
+  }
+  saveWorkspace(){
+    return 0
+  }
+  getWorksapce(uuid){
+    return 0
+  }
+  dump(){
+    return 0
+  }
+}
